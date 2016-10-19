c521bbe42f32fd5f458a18aa4e52e24755b47359
Caroline Verticchio <mcverticchio@gmail.com>
2016-10-18 21:00:53 -0400

    Updated background

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,13 +2,16 @@
 var Likes = require('./models/like.js').Likes;
 var $ = require('jquery');
 
-
-var likes = new Likes();
+//DOM Ready
+var likes = new Likes();                      //instantiates the "Likes"
 // console.log(likes.get('numlikes'));
 // console.log(likes.increaseLikesButton());
 
-$('.likeButton').on('click', function(event){
-  event.preventDefault();
+$('.likeButton').on('click', function(e){
+  e.preventDefault();
   // console.log(likes.increaseLikesButton());
   likes.increaseLikesButton();
 });
+
+
+//console.log(likes.toJSON)
